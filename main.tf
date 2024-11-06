4b98ec4be7717f8bff892a359b2b8adb95f3642c
Laércio Bubiak <lbubiak@hotmail.com>
2024-11-06 15:06:45 -0300

    config custom ports in firewall

--- main.tf
+++ main.tf
@@ -5,6 +5,20 @@ provider "google" {
   zone    = var.provider-zone
 }
 
+#Regra de firewall para permitir tráfego nas portas desejadas
+resource "google_compute_firewall" "allow_http_https_custom" {
+  name    = var.resource-firewall-name
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]  #Verifique se pode deixar para qualquer um acessar
+  target_tags   = ["http-server", "https-server", "custom-server"]
+}
+
 resource "google_compute_instance" "default" {
   name         = var.vm-name 
   machine_type = var.vm-machine_type
@@ -21,6 +35,5 @@ resource "google_compute_instance" "default" {
     network = "default"
     access_config {}
   }
+  tags = ["http-server", "https-server", "custom-server"]
 }
-
-
